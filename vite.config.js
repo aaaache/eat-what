14b642d4d039fc5aed0f3257d68e780d7e9b8bc2
zhang_gm@git <1424834523@qq.com>
2022-09-30 16:25:58 +0800

    feat: 今天中午吃什么

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.join(__dirname, "./src"),
+    },
+  },
+  plugins: [vue()],
+  server: {
+    port: 3000, // 打开的端口号
+    open: true, // 运行时打开浏览器
+  },
+});
