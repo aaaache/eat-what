c472e8b5910d8e9192cc4561cd2bd3fe0a6090a5
fadeache <1424834523@qq.com>
2023-03-31 13:29:30 +0800

    fix: base配置

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig ,loadEnv} from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.join(__dirname, "./src"),
+export default defineConfig(({ command, mode }) => {
+  return {
+    base: loadEnv(mode, process.cwd()).VITE_APP_PATH,
+    resolve: {
+      alias: {
+        "@": path.join(__dirname, "./src"),
+      },
     },
-  },
-  plugins: [vue()],
-  server: {
-    port: 3000, // 打开的端口号
-    open: true, // 运行时打开浏览器
-  },
+    plugins: [vue()],
+    server: {
+      port: 3000, // 打开的端口号
+      open: true, // 运行时打开浏览器
+    },
+  }
 });
